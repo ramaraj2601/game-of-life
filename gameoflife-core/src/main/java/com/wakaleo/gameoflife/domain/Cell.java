5392429c0e3f6c3a03350e5898afb5cdd4f5fd71
ramaraj2601 <ramaraj2601@gmail.com>
2013-05-21 16:07:52 +0530

    changed the special character

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,12 +1,13 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
-
+    LIVE_CELL("%"), DEAD_CELL(".");
+   
     private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
+      
     }
 
     @Override
